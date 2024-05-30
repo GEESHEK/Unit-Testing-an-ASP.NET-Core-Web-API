1793c0b0b953a8f718c172d16f619d0f7850e128
Gee Chong Shek <geeshek95@outlook.com>
2024-05-30 15:52:02 +0100

    First test written and test folder created

---
+++ EmploymentManagement.Test/EmployeeFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+
+namespace EmploymentManagement.Test;
+
+public class EmployeeFactoryTests
+{
+    [Fact]
+    //name of method being tested_Scenario under which it's being tested_expected behavior
+    public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500()
+    {
+        //Arrange: setting up the test
+        var employeeFactory = new EmployeeFactory();
+        
+        //Act: executing the actual test
+        var employee = (InternalEmployee)employeeFactory
+            .CreateEmployee("Kevin", "Dockx");
+        
+        //Assert: verifying the executed action
+        Assert.Equal(2500, employee.Salary);
+    }
+}
--- EmploymentManagement.Test/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace EmploymentManagement.Test;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
