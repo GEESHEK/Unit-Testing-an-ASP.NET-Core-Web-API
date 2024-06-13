5ca30763ecdf69b55d9090bbf43a3eb0a484263b
Gee Chong Shek <geeshek95@outlook.com>
2024-06-13 14:35:47 +0100

    Asserting on floating points with precision

--- EmployeeManagement.Test/EmployeeFactoryTests.cs
+++ EmployeeManagement.Test/EmployeeFactoryTests.cs
@@ -49,5 +49,19 @@ public class EmployeeFactoryTests
         Assert.InRange(employee.Salary, 2500, 3500);
     }
     
+    [Fact]
+    public void CreateEmployee_ConstructInternalEmployee_SalaryMustBe2500_PrecisionExample()
+    {
+        //Arrange: setting up the test
+        var employeeFactory = new EmployeeFactory();
+        
+        //Act: executing the actual test
+        var employee = (InternalEmployee)employeeFactory
+            .CreateEmployee("Kevin", "Dockx");
+        employee.Salary = 2500.123m;
+        
+        //Assert: verifying the executed action
+        Assert.Equal(2500, employee.Salary, 0);
+    }
     
 }
